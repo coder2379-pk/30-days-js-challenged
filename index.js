a06ed71a7048428c9880b9920af44fddeee4e4f5
coder2379-pk <coder2379@gmail.com>
2025-11-20 18:48:19 +0500

    Day 1 upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//  1st programme
+     
+// first method
+    console.log("Hello World");
+
+  // second method
+
+   alert("Hello World"); 
+
+
+  // third method 
+
+   document.write("Hello World");
