7bf5de97efebb3a667a60ccb2b48cf9c122c2e37
coder2379-pk <coder2379@gmail.com>
2025-11-21 21:17:19 +0500

    Added Program #2 in Day 1 file

--- index.js
+++ index.js
@@ -10,4 +10,26 @@
 
   // third method 
 
-   document.write("Hello World");
+   document.write("Hello World"); 
+
+
+  //   Day no 2 
+
+  // Our Main Goal:
+  //  Program to add Static numbers.
+  //  Program to add dynamic numbers.
+  
+  
+
+  // let firstNum = 10; 
+  // let secondNum = 20; 
+  // let add = firstNum + secondNum;
+  // console.log("Sum of two numbers:", add);
+
+
+  // dynamic Numbers
+  let firstNum = Number(prompt("enter first Number")); 
+  let secondNum = Number(prompt("enter your second number:")); 
+  let add = firstNum + secondNum;
+  console.log("Sum of two numbers:", add);
+  
